66ed8487b767ec08f99a6c762633f6784baaa741
suryaprakashf <146466761+suryaprakashf@users.noreply.github.com>
2024-08-06 15:46:04 +0530

    'new'

---
+++ game/app.js
@@ -0,0 +1,68 @@
+/*console.log("surya")
+
+let a=10;
+let b=20;
+let c=a+b;
+let d=a-b;
+let e=a*b;
+let f=a/b;
+let n="surya";
+let m="prakash";
+let o=n+m;
+console.log(c,d,e,f,o);*/
+
+
+/*let a=10;
+let b=20;
+if(a>b)
+{
+    console.log(a);
+}
+else{
+    console.log(b)
+}*/
+ 
+/*for(let i=0; i>=10;i++){
+    console.log(i)
+}*/
+
+/*let i=5;
+do{
+    console.log(i)
+    i++
+}
+while(i<=10)*/
+
+/*let studentname=[1,2,3,4,5,6]
+let arr= []
+studentname.filter((item) => {
+    if ((item%2)==0){
+        arr.push(item)
+        console.log("sdfgh",arr)
+    }
+    else{
+     console.log("array",arr);
+    }
+}
+)*/
+
+/*let number = [1,2,3,4,5];
+let arr =[10,20,30,40,50,60,70];
+number.filter((item) =>{
+    if(item%3==0){
+        arr.push()
+        console.log("value",arr)
+}
+else{
+    arr.pop()
+}
+console.log("surya",arr);
+
+}
+)*/
+
+const dept =['CSE','ECE','IT'];
+dept.splice(1,2,"surya","kavin")
+console.log(dept);
+
+const arr=[1,2,3];
